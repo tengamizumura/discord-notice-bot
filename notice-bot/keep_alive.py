08e91893af3d74d8062b7072633580f1f0fba35c
tengamizumura <xyztenga@gmail.com>
2026-04-08 21:30:23 +0900

    IPv6

--- notice-bot/keep_alive.py
+++ notice-bot/keep_alive.py
@@ -10,7 +10,7 @@ def home():
 
 def run():
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='::', port=port)
 
 def keep_alive():
     t = Thread(target=run)
